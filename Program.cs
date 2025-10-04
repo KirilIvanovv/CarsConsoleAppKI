2b83b9de249ae843b27c853869368f93a9c482da
kirils <kirilivanovexe@gmail.com>
2025-10-04 17:03:37 +0300

    Add AddCar, AddCustomer,AddOrder method for data input

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ using var context = new AppDbContext();
 
 while (true)
 {
-    Console.WriteLine("Output all data: 1");
-    Console.WriteLine("Add car: 2");
-    Console.WriteLine("Add client: 3");
+    Console.WriteLine("1- Output all data");
+    Console.WriteLine("2- Add car");
+    Console.WriteLine("3- Add customer");
+    Console.WriteLine("4 - Add order");
 
     Console.WriteLine("Exit: 0");
     string userChoice = Console.ReadLine();
@@ -17,6 +18,15 @@ while (true)
     {
         case "1":
             ShowAllData(context);
+            break;
+        case "2":
+            AddCar(context);
+            break;
+        case "3":
+            AddCustomer(context);
+            break;
+        case "4":
+            AddOrder(context);
             break;
         case "0":
             return;
@@ -47,6 +57,7 @@ static void ShowAllData(AppDbContext context)
         Console.WriteLine($"ID: {car.Id}");
         Console.WriteLine($"Brand: {car.Brand}");
         Console.WriteLine($"Model: {car.Model}");
+        Console.WriteLine($"Year: {car.Year}");
         Console.WriteLine($"LicensePlate: {car.LicensePlate}");
         Console.WriteLine(" ");
     }
@@ -63,5 +74,70 @@ static void ShowAllData(AppDbContext context)
         Console.WriteLine($"TotalCost: {order.TotalCost}");
         Console.WriteLine(" ");
     }
+}
 
+static void AddCar(AppDbContext context)
+{
+    Console.Write("Input brand: ");
+    string brand = Console.ReadLine();
+    Console.Write("Input model: ");
+    string model = Console.ReadLine();
+    Console.Write("Input license plate: ");
+    string licensePlate = Console.ReadLine();
+    Console.Write("Input year: ");
+    int year = int.Parse(Console.ReadLine());
+    Console.Write("Input color: ");
+    string color = Console.ReadLine();
+
+    var newCar = new Car { Brand = brand, Model = model, LicensePlate = licensePlate, Year = year, Color = color };
+
+    context.Cars.Add(newCar);
+    context.SaveChanges();
+
+    Console.WriteLine("Car added");
+
+
+}
+
+static void AddCustomer(AppDbContext context)
+{
+    Console.Write("Input name: ");
+    string name = Console.ReadLine();
+    Console.Write("Input email: ");
+    string email = Console.ReadLine();
+    Console.Write("Input phone number: ");
+    string phone = Console.ReadLine();
+
+    var newCustomer = new Customer { Name = name, Email = email, Phone = phone };
+
+    context.Customers.Add(newCustomer);
+    context.SaveChanges();
+
+    Console.WriteLine("Customer added");
+}
+
+static void AddOrder(AppDbContext context)
+{
+    Console.Write("Input Car ID: ");
+    int carId = int.Parse(Console.ReadLine());
+    Console.Write("Input Customer ID: ");
+    int customerId = int.Parse(Console.ReadLine());
+    Console.Write("Input description: ");
+    string description = Console.ReadLine();
+    Console.Write("Input total cost: ");
+    decimal totalCost = decimal.Parse(Console.ReadLine());
+
+    var newOrder = new Order
+    {
+        CarId = carId,
+        CustomerId = customerId,
+        OrderDate = DateTime.Now, 
+        Description = description,
+        TotalCost = totalCost
+    };
+
+    context.Orders.Add(newOrder);
+    context.SaveChanges();
+
+    Console.WriteLine("Order added");
 }
