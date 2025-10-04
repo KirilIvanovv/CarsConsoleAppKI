3afa0ef5fbb29c395be65ea62dac34c3d48e5739
kirils <kirilivanovexe@gmail.com>
2025-10-04 14:50:15 +0300

    Add ShowAllData method to display cars, customers and orders

--- Program.cs
+++ Program.cs
@@ -4,8 +4,64 @@ using CarsConsoleApp.Data;
 
 using var context = new AppDbContext();
 
-var car1 = new Car { Brand = "VW", Model = "Passat", Year = 2009, LicensePlate = "JU6360", Color = "White-Gold" };
-var car2 = new Car { Brand = "VW", Model = "Touran", Year = 2003, LicensePlate = "HS8111", Color = "Metalic" };
+while (true)
+{
+    Console.WriteLine("Output all data: 1");
+    Console.WriteLine("Add car: 2");
+    Console.WriteLine("Add client: 3");
 
-context.Cars.AddRange(car1, car2);
-context.SaveChanges();
+    Console.WriteLine("Exit: 0");
+    string userChoice = Console.ReadLine();
+
+    switch (userChoice)
+    {
+        case "1":
+            ShowAllData(context);
+            break;
+        case "0":
+            return;
+        default:
+            Console.WriteLine("Error");
+            break;
+    }
+}
+
+static void ShowAllData(AppDbContext context)
+{
+    var customers = context.Customers.ToList();
+
+    foreach (var customer in customers)
+    {
+        Console.WriteLine($"ID: {customer.Id}");
+        Console.WriteLine($"Name: {customer.Name}");
+        Console.WriteLine($"Email: {customer.Email}");
+        Console.WriteLine($"Phone: {customer.Phone}");
+        Console.WriteLine(" ");
+    }
+    Console.WriteLine("--------");
+
+    var cars = context.Cars.ToList();
+
+    foreach (var car in cars)
+    {
+        Console.WriteLine($"ID: {car.Id}");
+        Console.WriteLine($"Brand: {car.Brand}");
+        Console.WriteLine($"Model: {car.Model}");
+        Console.WriteLine($"LicensePlate: {car.LicensePlate}");
+        Console.WriteLine(" ");
+    }
+    Console.WriteLine("--------");
+
+    var orders = context.Orders.ToList();
+
+    foreach (var order in orders)
+    {
+        Console.WriteLine($"CarID: {order.CarId}");
+        Console.WriteLine($"CustomerID: {order.CustomerId}");
+        Console.WriteLine($"OrderDate: {order.OrderDate}");
+        Console.WriteLine($"Description: {order.Description}");
+        Console.WriteLine($"TotalCost: {order.TotalCost}");
+        Console.WriteLine(" ");
+    }
+
+}
